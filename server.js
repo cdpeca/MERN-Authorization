1d1f25f26781ee1a2e7b95730e90ae304100b3f2
cdpeca <chrispeca+github@hotmail.com>
2021-01-11 20:04:05 -0600

    fix issues with user route declaration pointing to users instead of user

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 
 // Routes
-app.use("./routes/api/users", users);
+app.use(".routes/api/users", user);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
