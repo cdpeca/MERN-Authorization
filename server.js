4fd165ea37dbeef881f3d695e97eec5f7a64373d
cdpeca <chrispeca+github@hotmail.com>
2021-01-11 19:48:45 -0600

    messed up in server.js

--- server.js
+++ server.js
@@ -28,6 +28,15 @@ mongoose
     .then(() => console.log("MongoDB successfully connected"))
     .catch(err => console.log(err));
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("./routes/api/users", users);
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
