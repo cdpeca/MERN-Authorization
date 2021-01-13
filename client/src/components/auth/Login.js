e986adfa4beb21c1a33be033389336ca71d24a8f
cdpeca <chrispeca+github@hotmail.com>
2021-01-13 10:28:32 -0600

    linked Redux to Login component and enabled displaying errors in Login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import classnames from "classnames";
+
 
 class Login extends Component {
     constructor() {
@@ -9,6 +14,18 @@ class Login extends Component {
             password: "",
             errors: {}
         };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.auth.isAuthenticated) {
+            this.props.history.push("/dashboard"); // push user to dashboard when they login
+        }
+
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+            });
+        }
     }
 
     onChange = e => {
@@ -23,7 +40,7 @@ class Login extends Component {
             password: this.state.password
         };
 
-        console.log(userData);
+        this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this props.history as a parameter
     };
 
     render() {
@@ -53,8 +70,15 @@ class Login extends Component {
                                     id="email"
                                     type="email"
                                     autoComplete="username"
+                                    className={classnames("", {
+                                        invalid: errors.email || errors.emailnotfound
+                                    })}
                                 />
                                 <label htmlFor="email">Email</label>
+                                <span className="red-text">
+                                    {errors.email}
+                                    {errors.emailnotfound}
+                                </span>
                             </div>
                             <div className="input-field col s12">
                                 <input
@@ -64,8 +88,15 @@ class Login extends Component {
                                     id="password"
                                     type="password"
                                     autoComplete="current-password"
+                                    className={classnames("", {
+                                        invalid: errors.password || errors.passwordincorrect
+                                    })}
                                 />
                                 <label htmlFor="password">Password</label>
+                                <span className="red-text">
+                                    {errors.password}
+                                    {errors.passwordincorrect}
+                                </span>
                             </div>
                             <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                                 <button
@@ -89,4 +120,18 @@ class Login extends Component {
     }
 }
 
-export default Login;
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(
+    mapStateToProps,
+    { loginUser }
+)(Login);
