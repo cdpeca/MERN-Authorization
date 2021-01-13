ab9ae6877ac6b3c45c1a3d43c97712808c07eb80
cdpeca <chrispeca+github@hotmail.com>
2021-01-13 12:01:25 -0600

    added functionality to main App.js to check for an active authentication token

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,40 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
-import {Provider } from "react-redux";
+
+import { Provider } from "react-redux";
 import store from "./store";
+
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds for comparison against expiration
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+  }
+}
+
 
 function App() {
   return (
@@ -18,6 +45,9 @@ function App() {
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
         </div>
       </Router>
     </Provider>
