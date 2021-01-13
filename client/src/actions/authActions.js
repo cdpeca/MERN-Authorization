8a3431daa22023fd8c11b507434c5b622ce209f0
cdpeca <chrispeca+github@hotmail.com>
2021-01-13 08:34:34 -0600

    bug: fixed typo with import of types, fixed malformed function definition for logoutUser

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -6,7 +6,7 @@ import {
     GET_ERRORS,
     SET_CURRENT_USER,
     USER_LOADING
-} from "./types;
+} from "./types";
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -55,7 +55,7 @@ export const setCurrentUser = decoded => {
 };
 
 // Log user out
-export const logoutUser = () dispatch => {
+export const logoutUser = () => dispatch => {
     // Remove token from local storage
     localStorage.removeItem("jwtToken");
     // Remove auth header for future requests
