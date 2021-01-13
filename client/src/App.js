739c7f14250b652daf3764936421421d6871c1fc
cdpeca <chrispeca+github@hotmail.com>
2021-01-13 12:16:02 -0600

    bug: added missing Switch import from react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
+
 import { setCurrentUser, logoutUser } from "./actions/authActions";
-
-
 import { Provider } from "react-redux";
 import store from "./store";
-
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
