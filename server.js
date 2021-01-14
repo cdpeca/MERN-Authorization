715cb6158f01576d8a5573495060abfc9d39a194
cdpeca <chrispeca+github@hotmail.com>
2021-01-13 20:41:40 -0600

    updated server for  heroku deployments

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const user = require("./routes/api/users");
 
@@ -47,6 +48,16 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", user);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirnam, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
